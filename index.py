a34f7b337bc0aca57c29b0ff8196a6ae6d217a63
Kirill Panchencko <videokirills@gmail.com>
2022-02-26 00:07:33 +0200

    textarea created and for adding notes will be used a new page

--- index.py
+++ index.py
@@ -9,6 +9,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/new_note")
+def new_note():
+    return render_template("new_note.html")
+
+
 @app.route("/notes")
 def notes():
     return render_template("notes.html")
